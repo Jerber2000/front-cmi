89944fe1d54d3e757df455364b909d9ee77500bf
Jerber <jcintor@miumg.edu.gt>
2025-07-28 22:10:08 -0600

    Cambios en la ruta en auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
 import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'https://back-cmi-production.up.railway.app/api/auth';;
+  private apiUrl = `${environment.apiUrl}/auth`;
 
   // ✅ AGREGAR para mensaje de bienvenida
   private showWelcomeSubject = new Subject<boolean>();
