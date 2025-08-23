6586cb05a500022f127e666a9497cb8d65026fb9
Jerber <jcintor@miumg.edu.gt>
2025-08-22 18:33:16 -0600

    actualizacion front

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
   //environment.ts/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://back-cmi-production.up.railway.app/api'
+  apiUrl: 'https://back-production-280a.up.railway.app/api/auth/login'
+  //https://back-cmi-production.up.railway.app/api
+
 };
