9573ff35020ba69fa93b8b7379d8881b2ab05150
Jerber <jcintor@miumg.edu.gt>
2025-09-14 16:57:20 -0600

    seguimient

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 //environments/environment.prod.ts
+
 export const environment = {
   production: true,  
-  apiUrl: 'https://back-production-280a.up.railway.app'
+  apiUrl: 'https://back-production-280a.up.railway.app/api',  
 };
