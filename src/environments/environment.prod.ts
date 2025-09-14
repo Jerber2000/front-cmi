00fd8796407ce8d55ef0a859f1254b4dec07a16c
Jerber <jcintor@miumg.edu.gt>
2025-09-14 11:21:49 -0600

    cambios para debug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: true,
   // ✅ USAR LA URL CORRECTA DEL BACKEND EN RAILWAY
-  apiUrl: 'https://back-production-280a.up.railway.app/api',
+  apiUrl: 'https://back-production-280a.up.railway.app',
    appName: 'CMI - Producción',
   enableLogging: false
 };
